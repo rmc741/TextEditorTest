5a89455c227ab25685110138113e7284a7ecd3d1
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-10-11 22:53:57 -0300

    ajustes header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,15 +1,23 @@
-import { AppBar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import "./index.css"
+import { Link } from "react-router-dom";
 
 export function Header() {
     return (
         <AppBar sx={{display:"flex"}}>
-            <a className="logo" href="/">
-                <img src="teste.svg" style={{height:"50px"}} alt="Logo NomeProjeto"/>
-                <Typography>
-                TEXTE EDITOR TEST
-                </Typography>
-            </a>
+            <Toolbar sx={{justifyContent:"space-between"}}>
+                <a className="logo" href="/">
+                    <img src="teste.svg" style={{height:"50px"}} alt="Logo NomeProjeto"/>
+                    <Typography>
+                        TEXTE EDITOR TEST
+                    </Typography>
+                </a>
+
+                <div className="nav-bar">
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="/about">About</Link>
+                </div>
+            </Toolbar>
         </AppBar>
     );
 }
